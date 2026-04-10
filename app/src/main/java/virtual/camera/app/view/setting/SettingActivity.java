73cb1667955d404d254a98d910f03f4c81f5c632
Jasserbelloumi <jassevjassev@gmail.com>
2026-04-10 04:24:34 +0100

    Add missing SettingActivity.java

---
+++ app/src/main/java/virtual/camera/app/view/setting/SettingActivity.java
@@ -0,0 +1,11 @@
+package virtual.camera.app.view.setting;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
