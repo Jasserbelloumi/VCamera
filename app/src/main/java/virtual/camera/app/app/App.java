c10e913167a7b17f8ede3b6548d4e6538d411f1c
Jasserbelloumi <jassevjassev@gmail.com>
2026-04-10 03:48:38 +0100

    Add missing App.java Application class

---
+++ app/src/main/java/virtual/camera/app/app/App.java
@@ -0,0 +1,24 @@
+package virtual.camera.app.app;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+    private static App instance;
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        context = getApplicationContext();
+    }
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
