515dfe8152415c107400183cc14a01dc798c46dc
Jasserbelloumi <jassevjassev@gmail.com>
2026-04-10 04:28:55 +0100

    Fix WelcomeActivity - use ComponentName instead of class ref

--- app/src/main/java/virtual/camera/app/view/main/WelcomeActivity.java
+++ app/src/main/java/virtual/camera/app/view/main/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package virtual.camera.app.view.main;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,9 +10,18 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-            finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(
+                    "virtual.camera.app",
+                    "virtual.camera.app.view.main.MainActivity"
+                ));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
         }, 1500);
     }
 }
