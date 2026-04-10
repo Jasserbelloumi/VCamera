7c1eeac0e2f09a899386cf4d674fd6dd9cd766bd
Jasserbelloumi <jassevjassev@gmail.com>
2026-04-10 04:24:32 +0100

    Add missing ListActivity.java

---
+++ app/src/main/java/virtual/camera/app/view/list/ListActivity.java
@@ -0,0 +1,11 @@
+package virtual.camera.app.view.list;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
