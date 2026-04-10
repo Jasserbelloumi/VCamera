f929be99e0528c02e39f10dc293ab8e417f68c5a
Jasserbelloumi <jassevjassev@gmail.com>
2026-04-10 04:24:33 +0100

    Add missing GmsManagerActivity.java

---
+++ app/src/main/java/virtual/camera/app/view/gms/GmsManagerActivity.java
@@ -0,0 +1,11 @@
+package virtual.camera.app.view.gms;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GmsManagerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
